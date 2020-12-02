7b871172dd0cc6de6ee0c5c6ed678257ffc6df1b
Gregoire De Fleurac <45977768+gregouz3@users.noreply.github.com>
2020-12-02 12:06:36 +0100

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -3,14 +3,22 @@ function editNav() {
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "topnav";//when close burger
   }
+  console.log(x.className);
+
 }
+
+editNav();
+
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+//btn close
+const closeBtn = document.querySelectorAll(".close");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +28,10 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Launch btn-close event
+closeBtn.forEach((close) => close.addEventListener("click", closeModal));
 
+//close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
