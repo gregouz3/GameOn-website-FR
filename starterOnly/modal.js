f86d8c9c30d7080908e824341f8c0a8704efb25e
Gregoire De Fleurac <45977768+gregouz3@users.noreply.github.com>
2020-12-03 16:24:48 +0100

    2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -6,10 +6,11 @@ function editNav() {
     x.className = "topnav";//when close burger
   }
   console.log(x.className);
+  console.log(document.reserve.location.value);
+
 
 }
 
-editNav();
 
 
 // DOM Elements
@@ -22,7 +23,6 @@ const closeBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
@@ -30,8 +30,26 @@ function launchModal() {
 
 // Launch btn-close event
 closeBtn.forEach((close) => close.addEventListener("click", closeModal));
-
 //close modal form
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+function validate() {
+  const form = document.reserve;
+  const email = document.getElementById("email").value;
+  console.log(form.quantity.value);
+  if (checkEmail(email) && form.birthdate.value !== null &&form.quantity.value == 0 )
+    return true;
+  if (checkEmail(email) && form.birthdate.value !== ""&&form.location.value !== "" )
+    return true;
+  else {
+    console.log('retry ');
+    return false;
+  }
+
+}
+function checkEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
