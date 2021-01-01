884a4ac50181a4ebcc29f13514dbec04a2eca440
Gregoire De Fleurac <45977768+gregouz3@users.noreply.github.com>
2021-01-01 16:39:08 +0100

    w3c, com

--- docs/modal.js
+++ docs/modal.js
@@ -10,8 +10,6 @@ function editNav() {
   console.log(document.reserve.checkbox1.checked);
 }
 
-
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -19,14 +17,12 @@ const formData = document.querySelectorAll(".formData");
 //btn close
 const closeBtn = document.querySelectorAll(".close");
 
-
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 // Launch btn-close event
 closeBtn.forEach((close) => close.addEventListener("click", closeModal));
 //close modal form
@@ -34,9 +30,8 @@ function closeModal() {
   modalbg.style.display = "none";
   window.location.reload();
 }
-
+// f validate form 
 function validate() {
-  
   const form = document.reserve;
   const smallFirst = document.getElementById("first").nextElementSibling;
   const smallLast = document.getElementById("last").nextElementSibling;
@@ -47,21 +42,27 @@ function validate() {
   const smallL = document.getElementById("v").nextElementSibling;
   const smallcheck = document.getElementById("vv").nextElementSibling;
 
+  // validate first name input
   if (form.first.value == "" || form.first.value.length < 2) {
+    // mess error injected <small>
     smallFirst.innerHTML = "Vous devez rentrer votre prénom  (au minimun 2 lettres)."
+    // on masque les autres messages d'erreurs
     smallLast.innerHTML = "";
     small.innerHTML = "";
     smallHb.innerHTML = "";
     smallQt.innerHTML = "";
     smallL.innerHTML = "";
     smallcheck.innerHTML = "";
+    // color et bordure red pour l'erreur
     form.first.style.border = "2px solid #fe142f";
+    // on masque les autres décorations 
     form.last.style.border = "";
     form.email.style.border = "";
     form.birthdate.style.border = "";
     form.quantity.style.border = "";
     return false;
   } else {
+    // input valide, color green
     form.first.style.border = "2px solid #279e7a";
   }
   if (form.last.value == "" 
@@ -161,16 +162,17 @@ function validate() {
       form.quantity.value !== "" &&
       form.location.value !== "" && 
       form.checkbox1.checked == true 
-      || 
+      ||
       form.first.value !== "" && 
       form.last.value !== "" && 
       checkEmail(email) && 
       form.birthdate.value !== "" &&
+      // quand lme nb de tournoi == 0, pas obliger de selec une ville.
       form.quantity.value == "0" &&
       form.location.value == "" && 
       form.checkbox1.checked == true 
   ){
-   // document.getElementById('thx').innerHTML = '';
+   // Launch modal remerciement
     document.getElementById('thx').innerHTML = `
     <div>
           <div class="thxxx">
@@ -190,6 +192,7 @@ function validate() {
   }
 }
 
+// f test address mail input with REGEX.
 function checkEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
